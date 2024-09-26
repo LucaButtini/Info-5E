4617f7781bbe83b09f91cafa883ac7f86869d2d6
Luca Buttini <121725497+LucaButtini@users.noreply.github.com>
2024-09-26 10:50:30 +0200

    Add files via upload

--- Ripasso_FrontEnd/script.js
+++ Ripasso_FrontEnd/script.js
@@ -1,21 +1,51 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Riferimenti agli elementi del DOM
-    const confermaButton = document.getElementById('conferma');
-    const nomeInput = document.getElementById('nome');
-    const cognomeInput = document.getElementById('cognome');
-    // Aggiunge l'evento click al pulsante di conferma
-    confermaButton.addEventListener('click', () => {
-        const nome = nomeInput.value;
-        const cognome = cognomeInput.value;
-        if (nome && cognome) {
-            // Salva i valori nel localStorage
-            localStorage.setItem('nome', nome);
-            localStorage.setItem('cognome', cognome);
-            // Visualizza i dati nella console
-            console.log(`Nome: ${nome}`);
-            console.log(`Cognome: ${cognome}`);
-        } else {
-            console.log('Per favore, inserisci sia il nome che il cognome.');
-        }
-    });
+let user = {
+    name: "",
+    password: "",
+};
+
+let regLink = document.querySelector('#account');
+regLink.addEventListener("click", function () {
+
+    const widht = screen.width / 4; //25%
+    const heihgt = screen.height / 2; //50%
+    const left = (screen.width - widht) / 2;
+    const top = (screen.height - heihgt) / 2;
+
+    //apro la finestra del form
+    let finestra = window.open("", "", `width=${widht},height=${heihgt},left=${left},top=${top}`);
+
+    // Creazione del collegamento al foglio di stile
+    let formCss = finestra.document.createElement("link");
+    formCss.rel = "stylesheet";
+    formCss.href = "styleForm.css";
+    finestra.document.head.appendChild(formCss);
+
+    // Creazione del form
+    let form = finestra.document.createElement("form");
+    form.classList = "container";
+
+    // Creazione del campo Username
+    let userName = finestra.document.createElement("input");
+    userName.type = "text";
+    userName.placeholder = "NAME";
+    userName.required = true;
+
+    // Creazione del campo Password
+    let pass = finestra.document.createElement("input");
+    pass.type = "password";
+    pass.placeholder = "PASSWORD";
+    pass.required = true;
+
+    // Creazione del pulsante di invio
+    let submit = finestra.document.createElement("input");
+    submit.type = "submit";
+    submit.value = "INVIA DATI";
+    submit.classList = "login";
+
+    // Aggiunta del form alla finestra e dei campi al form
+    finestra.document.body.appendChild(form);
+    form.appendChild(userName);
+    form.appendChild(pass);
+    form.appendChild(finestra.document.createElement("br"));
+    form.appendChild(submit);
 });
