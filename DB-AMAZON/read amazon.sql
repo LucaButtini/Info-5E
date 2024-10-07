5b6dab90d466e45a1eb4cd689264ad8af2d353fa
Luca Buttini <121725497+LucaButtini@users.noreply.github.com>
2024-10-07 12:19:44 +0200

    Update read amazon.sql

--- DB-AMAZON/read amazon.sql
+++ DB-AMAZON/read amazon.sql
@@ -13,3 +13,28 @@ where p.famiglia ="";
 select p.famiglia, p.marca
 from db_amazon.prodotti p 
 where p.famiglia is null;
+
+/*Ricerchiamo chi ha la stessa marca,
+ * Questo database è case Insensitive
+ * se volessi posso che sia case sensitive posso modificare dal dbvear
+ * */
+select p.descrizione, p.marca 
+from db_amazon.prodotti p 
+where p.marca="TICINO-ABB";
+
+
+/*operatori di confronto fra le stringhe*/
+/*
+ * opearatore "like" confronta se una stringa assomiglia ad un'altra
+ * */
+
+/*marche che iniziano con la lettera t*/
+select p.descrizione, p.marca
+from db_amazon.prodotti p 
+-- where p.descrizione like "q%";
+-- where p.marca like "T%";
+where p.descrizione like "q%" and p.marca  like "T%";
+
+-- aggiornare il campo descrizione eliminando tutti gli spazi
+update db_amazon.prodotti p
+set p.descrizione = trim(p.descrizione); -- toglie gli spazi 
