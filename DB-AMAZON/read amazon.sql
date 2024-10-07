e06eb362e4d5cca5d2d3a15ba41dc99a4e62b80b
Luca Buttini <121725497+LucaButtini@users.noreply.github.com>
2024-10-07 11:47:00 +0200

    Add files via upload

---
+++ DB-AMAZON/read amazon.sql
@@ -0,0 +1,11 @@
+select * 
+from db_amazon.prodotti p ;
+
+select p.famiglia, p.marca
+from db_amazon.prodotti p 
+where p.famiglia <>"";
+
+/*Cambia il valore nelle colonne vuote in null*/
+update db_amazon.prodotti p
+set p.famiglia = null 
+where p.famiglia ="";
