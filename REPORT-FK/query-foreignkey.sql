373678299ecffe606bc0814edf55e202e43d8681
Luca Buttini <121725497+LucaButtini@users.noreply.github.com>
2024-11-15 12:41:25 +0100

    chiave esterna

---
+++ REPORT-FK/query-foreignkey.sql
@@ -0,0 +1,51 @@
+use uniro;
+
+select *
+from uniro.report r ;
+
+create table uniro.studenti(
+matricola_studente int primary key,
+nome varchar(50),
+cognome varchar(50),
+data_nascita date
+);
+
+create table uniro.insegnamenti(
+codice_insegnamento int primary key, 
+nome_insegnamento varchar(50),
+crediti int
+);
+
+create table uniro.esami(
+matricola_studente int,
+codice_insegnamento int,
+id_esame int primary key auto_increment,
+data_esame datetime,
+voto int,
+lode boolean,
+foreign key (matricola_studente) references uniro.studenti(matricola_studente),
+foreign key (codice_insegnamento) references uniro.insegnamenti(codice_insegnamento)
+);
+
+
+insert into uniro.studenti(matricola_studente, nome, cognome, data_nascita)
+select distinct r.matricola_studente , r.nome , r.cognome , r.datadinascita 
+from uniro.report r;
+
+select *
+from uniro.studenti;
+
+insert into uniro.insegnamenti (codice_insegnamento, nome_insegnamento, crediti)
+select distinct r.codice_insegnamento, r.codice_insegnamento , r.crediti
+from uniro.report r;
+
+select *
+from uniro.insegnamenti i;
+
+insert into uniro.esami(matricola_studente, codice_insegnamento, data_esame, voto, lode)
+select r.matricola_studente, r.codice_insegnamento, r.data, r.voto, r.lode
+from uniro.report r;
+
+select *
+from uniro.esami e;
+
