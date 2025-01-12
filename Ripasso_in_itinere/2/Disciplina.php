5aad46eec5106c1486a667e1f25db5a70184b0bb
LucaButtini <luca.buttini@iisviolamarchesini.edu.it>
2025-01-12 23:19:17 +0100

    es 2 inizio

--- Ripasso_in_itinere/2/Disciplina.php
+++ Ripasso_in_itinere/2/Disciplina.php
@@ -1,39 +1,47 @@
 <?php
 
-
-class Disciplina
-{
+class Disciplina {
     private string $nome;
     private array $percorsi = [];
 
-    public function __construct(string $nome, array $percorsi) {
+    public function __construct(string $nome, array $percorsi = []) {
         $this->nome = $nome;
         $this->percorsi = $percorsi;
     }
 
-    public function getPercorsi(): array
-    {
+    public function getPercorsi(): array {
         return $this->percorsi;
     }
 
-    public function setPercorsi(array $percorsi): void
-    {
+    public function setPercorsi(array $percorsi): void {
         $this->percorsi = $percorsi;
     }
 
-    public function getNome(): string
-    {
+    public function getNome(): string {
         return $this->nome;
     }
 
-    public function setNome(string $nome): void
-    {
+    public function setNome(string $nome): void {
         $this->nome = $nome;
     }
 
-    //metodi
+    // Metodi
+    public function addPercorso(Percorso $nuovoPercorso): bool {
+        foreach ($this->percorsi as $percorso) {
+            if ($percorso->equals($nuovoPercorso)) {
+                return false; // Il percorso esiste già
+            }
+        }
+        $this->percorsi[] = $nuovoPercorso;
+        return true;
+    }
 
-
-
-
+    public function find(string $path): ?Percorso {
+        foreach ($this->percorsi as $percorso) {
+            if($percorso->getNome() == $path) {
+                return $percorso;
+            }
+        }
+        return null;//non trovato
+    }
 }
--- Ripasso_in_itinere/2/Percorso.php
+++ Ripasso_in_itinere/2/Percorso.php
@@ -2,37 +2,42 @@
 
 class Percorso
 {
+    private string $path;
     private string $argomento;
     private string $mese;
 
-    public function __construct(string $argomento, string $mese){
+    public function __construct(string $path, string $argomento, string $mese) {
+        $this->path = $path;
         $this->argomento = $argomento;
         $this->mese = $mese;
     }
 
-    public function getMese(): string
-    {
-        return $this->mese;
+    public function getPath(): string {
+        return $this->path;
     }
 
-    public function setMese(string $mese): void
-    {
-        $this->mese = $mese;
-    }
-
-    public function getArgomento(): string
-    {
+    public function getArgomento(): string {
         return $this->argomento;
     }
 
-    public function setArgomento(string $argomento): void
-    {
+    public function getMese(): string {
+        return $this->mese;
+    }
+
+    public function setPath(string $path): void {
+        $this->path = $path;
+    }
+
+    public function setArgomento(string $argomento): void {
         $this->argomento = $argomento;
     }
 
-    //metodi
+    public function setMese(string $mese): void {
+        $this->mese = $mese;
+    }
 
-
-
-
+   // controllo se ci sono paths uguali
+    public function checkPaths(Percorso $altroPercorso): bool {
+        return $this->path === $altroPercorso->getPath();
+    }
 }
