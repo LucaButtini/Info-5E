96659c28acd9a399012bb8e2df64d59af172ae7e
LucaButtini <luca.buttini@iisviolamarchesini.edu.it>
2025-01-30 22:12:44 +0100

    .

--- PHP/Libreria_bozza/header.php
+++ PHP/Libreria_bozza/header.php
@@ -13,9 +13,8 @@ $page = substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="icon" type="image/x-icon" href="Immagini/book.ico">
-
     <title><?=/**@var $title*/ $title?></title>
+    <link rel="icon" type="image/x-icon" href="Immagini/book.ico">
 </head>
 <body class="bg-body-tertiary">
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
