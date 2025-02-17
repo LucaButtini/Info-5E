c550eca728a7411b9aecda9bf4b7d60fcccea92a
Luca Buttini <121725497+LucaButtini@users.noreply.github.com>
2025-02-17 12:30:08 +0100

    Delete PHP/Progetti/Utopia_sovrani/Conf directory

--- PHP/Progetti/Utopia_sovrani/Conf/DBconfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'dns' => 'mysql:host=localhost;dbname=utopia',
-    'username' => 'root',
-    'password' => 'root',
-    'options' => [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ]
-];
