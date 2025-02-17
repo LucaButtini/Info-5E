c386de0e53c15a9cd18fb2d932af810f09211d9a
LucaButtini <luca.buttini@iisviolamarchesini.edu.it>
2025-02-17 16:33:38 +0100

    fixed

--- PHP/Progetti/Utopia_sovrani/Config/DBconfig.php
+++ PHP/Progetti/Utopia_sovrani/Config/DBconfig.php
@@ -3,6 +3,6 @@
 return [
     'dns' => 'mysql:host=localhost;dbname=utopia',
     'username' => 'root',
-    'password' => 'root',
+    'password' => '',
     'options' => [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ]
 ];
