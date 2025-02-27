b89262d9f7573d500bd0a1dfb3694e0dbc29ab54
LucaButtini <luca.buttini@iisviolamarchesini.edu.it>
2025-02-27 17:51:58 +0100

    ripasso

--- PHP/ripasso/per_ver/action.php
+++ PHP/ripasso/per_ver/action.php
@@ -2,34 +2,42 @@
 $albums=require_once 'albums.php';
 
 if($_SERVER['REQUEST_METHOD']=='POST'){
-$user=$_POST['user']??null;
-$password=$_POST['password']??null;
-$email=$_POST['email']??null;
-$ordini = $_POST['album'] ?? [];
-$quantita=$_POST['quantita']??[];
+    //prendo i dati dal form, se non sono settati metto valori di default
+    $user=$_POST['user']??null;
+    $password=$_POST['password']??null;
+    $email=$_POST['email']??null;
+    $ordini = $_POST['album'] ?? [];
+    $quantita=$_POST['quantita']??[];
 
-if(empty($user) || empty($password) || empty($email)){
-    echo 'devi inserire tutti i dati';
-    exit;
-}
-
-$ordiniUniti=[];
-
-foreach($ordini as $i=>$ord){
-    if($quantita[$i] != 0){
-        if(!isset($ordiniUniti[$ord])){
-            $ordiniUniti[$ord]=0;
-        }
-        $ordiniUniti[$ord] += $quantita[$i];
+    //controllo se variabile è empty
+    if(empty($user) || empty($password) || empty($email)){
+        echo 'devi inserire tutti i dati';
+        exit;
     }
-}
 
-if(empty($ordiniUniti)){
-    echo 'no ordini selezionati';
-    exit;
-}
+    //prodotti
+    $ordiniUniti=[];
+
+    //inserisco nell'array i prodotti che hanno quantita >0
+    foreach($ordini as $i=>$ord){
+        if($quantita[$i] != 0){
+            //se non è settata la quantita non viene inserito
+            if(!isset($ordiniUniti[$ord])){
+                $ordiniUniti[$ord]=0;
+            }
+            //sommo la quantità in modo da avere poi l'elenco ordinato
+            $ordiniUniti[$ord] += $quantita[$i];
+        }
+    }
+
+    //controllo se ordini è empty
+    if(empty($ordiniUniti)){
+        echo 'no ordini selezionati';
+        exit;
+    }
 
 }else{
+    //controllo se c'è errore nel prendere i dati dal form
 echo 'errore';
 exit;
 }
@@ -45,6 +53,7 @@ exit;
     <title>riepilogo</title>
 </head>
 <body>
+<!---stampo elenco dati personali-->
     <h2 style="color: red">DATI PERSONALI</h2>
     <h4><strong>User</strong></h4>
     <p><?= $user?></p>
@@ -53,6 +62,7 @@ exit;
     <h4><strong>Email</strong></h4>
     <p><?= $email?></p>
 <hr>
+<!---stampo ordini raggruppati-->
     <h4><strong>ordini</strong></h4>
     <?php foreach ($ordiniUniti as $id=>$quantita) {?>
         <p>[<?= $quantita ?>] <?= $albums[$id] ?></p>
--- PHP/ripasso/per_ver/index.php
+++ PHP/ripasso/per_ver/index.php
@@ -1,6 +1,7 @@
 <?php
+//prendo l'array contenente i tipi di album
 $albums=require_once 'albums.php';
-
+//numero album tipi in array
 $num= count($albums);
 
 //echo $num;
@@ -18,7 +19,9 @@ $num= count($albums);
 <body>
 <div style="text-align: center; font-size:large">
     <h1 style="color: red">RIPASSO</h1>
+    <!--form per inserimento dei dati-->
     <form action="action.php" method="post">
+        <!--inserimento nome, password e email -->
         <label for="user">Inserisci username:</label><br>
         <input type="text" required placeholder="user" name="user" id="user"><br><br>
         <label for="password">Inserisci password:</label><br>
@@ -26,9 +29,12 @@ $num= count($albums);
         <label for="email">Inserisci email:</label><br>
         <input type="email" required placeholder="nome@dominio.com" name="email" id="email"><br><br>
 
-        <?php  for($i=0; $i<$num; $i++) { ?>
-
-        <p style="color: blue">Scegli opzioni prodotto <?= $i+1?></p>
+        <!--creo i dropdown per la quantita di prodotti-->
+        <?php
+        //ciclo l'array
+        for($i=0; $i<$num; $i++) { ?>
+            <!--dropdown prodotti-->
+            <p style="color: blue">Scegli opzioni prodotto <?= $i+1?></p>
             <label for="<?= $i?>Tip">Tipologia</label><br>
             <select id="<?= $i?>Tip" name="album[]"><br><br>
                 <option hidden disabled selected value="">Seleziona</option>
@@ -38,6 +44,7 @@ $num= count($albums);
             </select>
 
         <br>
+            <!--inserisci quantità prodotti desiderata. Quantità la metto come array dato che ne ho una per ogni prodotto-->
         <label for="<?= $i?>Qu">quantità</label><br>
         <input type="number" id="<?= $i?>Qu" name="quantita[]" value="0" min="0"><br>
         <?php } ?>
